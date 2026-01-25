8ebe2278d0fa86c66fb121fe44bc0665a7bf6c05
Pumpkin <wishsmp1002@gmail.com>
2026-01-25 22:03:52 +1100

    Mace Control

--- src/main/java/net/macecontrol/Main.java
+++ src/main/java/net/macecontrol/Main.java
@@ -1,11 +1,7 @@
 package net.macecontrol;
 
-import net.macecontrol.managers.CombatManager;
 import net.macecontrol.managers.PluginDataManager;
 import net.macecontrol.managers.EnchantmentManager;
-import net.macecontrol.utils.DisableBedBombing;
-import net.macecontrol.utils.PotionRestrictions;
-import net.macecontrol.utils.TippedArrowRestrictions;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
